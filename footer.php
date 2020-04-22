68bb7b812900ce3053921ae7b682c476d2770ebc
caroline-ctrl <duon.caroline@gmail.com>
2020-04-22 10:32:35 +0200

    mise en place function ajouter_vue()

--- footer.php
+++ footer.php
@@ -2,7 +2,12 @@
 <footer><hr>
 
 <div class="row">
-    <div class="col-md-4"></div>
+    <div class="col-md-4">
+        <?php
+        require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions' . DIRECTORY_SEPARATOR . 'compteur.php';
+        ajouter_vue();
+        ?>
+    </div>
     <div class="col-md-4">
         <form action="/newsletter.php" method="POST">
             <div class="form-group">
--- functions/compteur.php
+++ functions/compteur.php
@@ -3,11 +3,14 @@
 function ajouter_vue ()
 {
     $fichier = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'compteur';
+    $compteur = 1;
     if (file_exists($fichier)){
         $compteur = (int)file_get_contents($fichier);
         $compteur++;
         file_put_contents($fichier, $compteur);
-    } else {
-        file_put_contents($fichier, '1');
     }
+    file_put_contents($fichier, $compteur);
+    
 }
+
+
