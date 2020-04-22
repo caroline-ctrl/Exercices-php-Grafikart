c9ba9624440aa68261900c460ea8f18d48afd2e0
caroline-ctrl <duon.caroline@gmail.com>
2020-04-22 10:52:03 +0200

    compteur par jour

--- functions/compteur.php
+++ functions/compteur.php
@@ -3,6 +3,13 @@
 function ajouter_vue ()
 {
     $fichier = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'compteur';
+    $fichier_journalier = $fichier . '-' . date('Y-m-d');
+    incrementer_compteur($fichier);
+    incrementer_compteur($fichier_journalier);
+}
+
+function incrementer_compteur(string $fichier)
+{
     $compteur = 1;
     if (file_exists($fichier)){
         $compteur = (int)file_get_contents($fichier);
@@ -10,8 +17,9 @@ function ajouter_vue ()
         file_put_contents($fichier, $compteur);
     }
     file_put_contents($fichier, $compteur);
-    
+
 }
+
 
 
 function nombre_vues(): string
